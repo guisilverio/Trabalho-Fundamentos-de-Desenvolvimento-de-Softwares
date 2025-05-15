649d91bb2712818ceb10e278098b05ce893631b4
guisilverio <Gui2016silverio@gmail.com>
2025-05-15 09:16:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function clique() {
+    const mensagem =
+        textomensagem.innerHTML =
+        "Obrigado por ler! Antes de sair quero recomendar um livro: Os Sete Maridos de Evelyn Hugo de Taylor Jenkins Reid, um romance  de tirar o folêgo, com uma protagonista carismática, lutadora e obstinada."
+
+}
